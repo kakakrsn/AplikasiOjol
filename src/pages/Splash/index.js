913c45f2200836190717b82fdd266da6de302c65
Kaka krsn <kakakarsana50@gmail.com>
2022-06-02 15:54:31 +0700

    create index for splash

---
+++ src/pages/Splash/index.js
@@ -0,0 +1,18 @@
+import React, {useEffect} from 'react';
+import { View, Text} from 'react-native';
+
+
+const Splash = ({navigation}) => {
+    useEffect(() => {
+        setTimeout(() => {
+            navigation.replace('WelcomeAuth');
+        }, 5000);
+    });
+    return (
+        <View>
+            <Text>Splash Screen</Text>
+        </View>
+    );
+}
+
+export default Splash;
